429fbd36ec81d50d0b282b7e0aab79d5a1e9e52a
Ignacio Navarro Blázquez <inavarroblazquez@gmail.com>
2021-05-14 21:02:56 +0200

    Create shout positive case

--- src/test/java/acme/testing/anonymous/shout/AnonymousCreateShoutTest.java
+++ src/test/java/acme/testing/anonymous/shout/AnonymousCreateShoutTest.java
@@ -25,27 +25,22 @@ public class AnonymousCreateShoutTest extends AcmeTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/anonymous/shout/CreatePositive.csv", encoding = "utf-8", numLinesToSkip = 1)
     @Order(10)
-    public void list(final int recordIndex, final String description, final String finish,final String start,
-    	final String is_public, final String link, final String title, final String workload) {
+    public void CreatePositive(final int recordIndex, final String author, final String info,final String moment,
+    	final String text) {
         super.clickOnMenu("Anonymous", "Shout!");        
         
-//        super.checkColumnHasValue(recordIndex, 0, title);
-//        super.checkColumnHasValue(recordIndex, 1, start);
-//        super.checkColumnHasValue(recordIndex, 2, finish);    
-//        super.checkColumnHasValue(recordIndex, 3, workload);
-//        super.checkColumnHasValue(recordIndex, 4, link);
-//        
-//        super.clickOnListingRecord(recordIndex);
-//        
-//        super.checkInputBoxHasValue("title", title);
-//        super.checkInputBoxHasValue("description", description);        
-//        super.checkInputBoxHasValue("public", is_public);
-//        super.checkInputBoxHasValue("workload", workload);
-//        super.checkInputBoxHasValue("start", start);
-//        super.checkInputBoxHasValue("finish", finish);
-//        super.checkInputBoxHasValue("link",link);
+
+        super.fillInputBoxIn("author", author);
+        super.fillInputBoxIn("text", text);
+        super.fillInputBoxIn("info", info);
+        super.clickOnSubmitButton("Shout!");
         
+        super.checkColumnHasValue(recordIndex, 0, moment);
+        super.checkColumnHasValue(recordIndex, 1, author);
+        super.checkColumnHasValue(recordIndex, 2, text);    
+        super.checkColumnHasValue(recordIndex, 3, info);
         
-        super.signOut();
+        super.clickOnListingRecord(recordIndex);
+        
     }
 }
