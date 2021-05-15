4dd948fae76b66eceeecf187fc63555e6d0cb1b1
Pedro Alonso Pontiga <pedropontiga@gmail.com>
2021-05-15 17:06:20 +0200

    Workplan create test positive and negative case

--- src/test/java/management/workplan/ManagementWorkplanCreateTest.java
+++ src/test/java/management/workplan/ManagementWorkplanCreateTest.java
@@ -5,37 +5,79 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.By;
 
-public abstract class ManagementWorkplanCreateTest extends AcmeTest {
+import java.util.List;
+
+class ManagementWorkplanCreateTest extends AcmeTest {
+
+    @Override
+    @BeforeAll
+    public void beforeAll() {
+        super.beforeAll();
+
+        super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
+        super.setAutoPausing(true);
+
+        this.signIn("administrator", "administrator");
+        super.clickAndGo(By.linkText("Administrator"));
+        super.clickAndGo(By.linkText("Populate DB (initial)"));
+        super.checkAlertExists(true);
+        super.clickAndGo(By.linkText("Administrator"));
+        super.clickAndGo(By.linkText("Populate DB (samples)"));
+        super.checkAlertExists(true);
+
+    }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/managament/workplan/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-    @Order(10)
-    public void createPositive(final int recordIndex,String title,String description, String isPublic,String startDate,String finishDate) {
-        super.signIn("juan21", "1234");
+    @Order(3)
+    public void createPositive(final int recordIndex, String title, String description, String isPublic, String startDate, String finishDate) {
+        super.signIn("rosa21", "1234");
 
-        super.clickOnMenu("Managament", "Create a workplan");
+        super.clickOnMenu("Management", "Create new Workplan");
 
         super.fillInputBoxIn("title", title);
         super.fillInputBoxIn("description", description);
-        super.fillInputBoxIn("isPublic", isPublic);
-        super.fillInputBoxIn("startDate", startDate);
-        super.fillInputBoxIn("finishDate", finishDate);
+        if(isPublic.equals("true"))super.clickAndGo(By.id("isPublic$proxy"));
+        super.clickAndGo(By.name("newTasksId"));
+        super.fill(By.id("startDateTime"), startDate);
+        super.fill(By.id("finishDateTime"), finishDate);
         super.clickOnSubmitButton("Create");
 
-        super.clickOnMenu("Managament", "List my workplans");
+
+        super.clickOnMenu("Management", "My Workplans List");
 
         super.checkColumnHasValue(recordIndex, 0, title);
-        super.checkColumnHasValue(recordIndex, 1, startDate);
-        super.checkColumnHasValue(recordIndex, 2, finishDate);
+        super.checkColumnHasValue(recordIndex, 2, startDate);
+        super.checkColumnHasValue(recordIndex, 3, finishDate);
 
         super.clickOnListingRecord(recordIndex);
 
         super.checkInputBoxHasValue("title", title);
         super.checkInputBoxHasValue("description", description);
         super.checkInputBoxHasValue("isPublic", isPublic);
-        super.checkInputBoxHasValue("startDate", startDate);
-        super.checkInputBoxHasValue("finishDate", finishDate);
+        super.checkInputBoxHasValue("startDateTime", startDate);
+        super.checkInputBoxHasValue("finishDateTime", finishDate);
+
+        super.signOut();
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/managament/workplan/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+    @Order(3)
+    public void createNegative(final int recordIndex, String title, String description, String isPublic, String startDate, String finishDate) {
+        super.signIn("rosa21", "1234");
+
+        super.clickOnMenu("Management", "Create new Workplan");
+
+        super.fillInputBoxIn("title", title);
+        super.fillInputBoxIn("description", description);
+        if(isPublic.equals("true"))super.clickAndGo(By.id("isPublic$proxy"));
+        super.clickAndGo(By.name("newTasksId"));
+        super.fill(By.id("startDateTime"), startDate);
+        super.fill(By.id("finishDateTime"), finishDate);
+        super.clickOnSubmitButton("Create");
+        super.checkErrorsExist();
 
         super.signOut();
     }
