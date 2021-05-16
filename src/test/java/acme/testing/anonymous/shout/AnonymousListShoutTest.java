718e0656ca8c2b841db7da73589707eac64d8ee9
Ignacio Navarro Blázquez <inavarroblazquez@gmail.com>
2021-05-16 20:02:27 +0200

    List working but no older than one month is causing problems

--- src/test/java/acme/testing/anonymous/shout/AnonymousListShoutTest.java
+++ src/test/java/acme/testing/anonymous/shout/AnonymousListShoutTest.java
@@ -2,21 +2,45 @@ package acme.testing.anonymous.shout;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import acme.testing.AcmeTest;
 
+@ExtendWith(MockitoExtension.class)
 public class AnonymousListShoutTest extends AcmeTest {
 
+	
+//	@Mock
+//	private AnonymousShoutRepository sv;
+	
+//	@InjectMocks
+//	private AnonymousShoutRepository sv;
+	
     // Lifecycle management ---------------------------------------------------
     @Override
     @BeforeAll
     public void beforeAll() {
         super.beforeAll();
 
+//        MockitoAnnotations.initMocks(this);
+        
         super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
         super.setAutoPausing(true);
+        
+//        final List<Shout> shouts = new ArrayList<>();
+//        final Shout s = new Shout();
+//        final Calendar c = Calendar.getInstance();
+//        c.set(2021, 4, 1);
+//        s.setAuthor("Fernandoedasredw");
+//        s.setMoment(c.getTime());
+//        s.setId(1);
+//        s.setText("Let's goos");
+//        shouts.add(s);
+////        Mockito.when(this.sv.findMany(ArgumentMatchers.<Request<Shout>>any())).thenReturn(shouts);
+//        Mockito.when(this.sv.findMany(ArgumentMatchers.any(Date.class))).thenReturn(shouts);
 
     }
     // Test cases -------------------------------------------------------------
