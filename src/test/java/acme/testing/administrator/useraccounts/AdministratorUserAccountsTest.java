99b6bb4710a966c51a34211028ffcc4ce7465027
Fernando <frabascol@gmail.com>
2021-05-22 11:43:37 +0200

    Positive Show & List User Accounts

--- src/test/java/acme/testing/administrator/useraccounts/AdministratorUserAccountsTest.java
+++ src/test/java/acme/testing/administrator/useraccounts/AdministratorUserAccountsTest.java
@@ -7,7 +7,7 @@ import org.springframework.core.annotation.Order;
 
 import acme.testing.AcmeTest;
 
-public class AdministratorUserAccountsListTest extends AcmeTest{
+public class AdministratorUserAccountsTest extends AcmeTest{
 
 	 // Lifecycle management ---------------------------------------------------
 	@Override
@@ -24,7 +24,7 @@ public class AdministratorUserAccountsListTest extends AcmeTest{
     @ParameterizedTest
     @CsvFileSource(resources = "/administrator/useraccount/listPositive.csv", encoding = "utf-8", numLinesToSkip = 1)
     @Order(10)
-    public void list(final int recordIndex, final String username, final String identity_name,
+    public void listPositive(final int recordIndex, final String username, final String identity_name,
     	final String identity_surname) {
     	this.signIn("administrator", "administrator");
         super.clickOnMenu("Administrator", "User accounts");        
@@ -32,6 +32,25 @@ public class AdministratorUserAccountsListTest extends AcmeTest{
         super.checkColumnHasValue(recordIndex,0 , username); 
 		super.checkColumnHasValue(recordIndex,1 , identity_name); 
 		super.checkColumnHasValue(recordIndex,2 , identity_surname); 
+    }
+    
+    
+    @ParameterizedTest
+    @CsvFileSource(resources = "/administrator/useraccount/showPositive.csv", encoding = "utf-8", numLinesToSkip = 1)
+    @Order(10)
+    public void showPositive(final int recordIndex, final String username, final String identity_name,
+    	final String identity_surname, final String identity_email, final String role, final String enabled) {
+    	this.signIn("administrator", "administrator");
+        super.clickOnMenu("Administrator", "User accounts");        
+        
+        super.clickOnListingRecord(recordIndex); 
+        
+        super.checkInputBoxHasValue("username", username); 
+        super.checkInputBoxHasValue("identity.name", identity_name); 
+        super.checkInputBoxHasValue("identity.surname", identity_surname); 
+        super.checkInputBoxHasValue("identity.email", identity_email); 
+        super.checkInputBoxHasValue("roleList", role); 
+        super.checkInputBoxHasValue("status",enabled); 
     }
     
 	
