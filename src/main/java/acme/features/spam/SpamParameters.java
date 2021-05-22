2ebe99c356de9d04f7a985969ec1b3637b18a30e
Ignacio Navarro Blázquez <inavarroblazquez@gmail.com>
2021-05-22 12:50:05 +0200

    Minor fix import spamParameters

--- src/main/java/acme/features/spam/SpamParameters.java
+++ src/main/java/acme/features/spam/SpamParameters.java
@@ -12,17 +12,20 @@
 
 package acme.features.spam;
 
-import acme.framework.entities.DomainEntity;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.validator.constraints.Range;
+import java.util.Collection;
+import java.util.Objects;
 
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.validator.constraints.Range;
+
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -48,18 +51,18 @@ public class SpamParameters extends DomainEntity {
 
 
 	@Override
-	public boolean equals(Object o) {
+	public boolean equals(final Object o) {
 		if (this == o) return true;
 		if (!(o instanceof SpamParameters)) return false;
 		if (!super.equals(o)) return false;
-		SpamParameters that = (SpamParameters) o;
-		return Objects.equals(threshold, that.threshold) &&
-				Objects.equals(keywords, that.keywords);
+		final SpamParameters that = (SpamParameters) o;
+		return Objects.equals(this.threshold, that.threshold) &&
+				Objects.equals(this.keywords, that.keywords);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(super.hashCode(), threshold, keywords);
+		return Objects.hash(super.hashCode(), this.threshold, this.keywords);
 	}
 
 
