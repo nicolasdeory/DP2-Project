a9226b4a57bd625bcf2f6906dd78211e00c6e028
Pedro Alonso Pontiga <pedropontiga@gmail.com>
2021-05-23 17:41:51 +0200

    Minor changes to ManagementDeleteTest

--- src/test/java/acme/testing/management/task/ManagementTaskDeleteTest.java
+++ src/test/java/acme/testing/management/task/ManagementTaskDeleteTest.java
@@ -47,7 +47,15 @@ public class ManagementTaskDeleteTest extends AcmeTest {
 
         super.clickOnListingRecord(recordIndex);
 
-        super.clickOnSubmitButton("Delete");
+        String path=super.getSimplePath();
+        String query=super.getContextQuery();
+        super.checkButtonExists("Delete");
+        super.clickOnReturnButton("Delete");
+
+        super.checkColumnDoesNotHaveValue(1, 0, title);
+
+        super.navigate(path,query);
+        super.checkErrorsExist();
 
         super.signOut();
     }
@@ -60,10 +68,23 @@ public class ManagementTaskDeleteTest extends AcmeTest {
         super.signIn("juan21", "1234");
         super.clickOnMenu("Management", "My tasks");
 
+        super.checkColumnHasValue(recordIndex, 0, title);
+        super.checkColumnHasValue(recordIndex, 1, description);
+        super.checkColumnHasValue(recordIndex, 2, start);
+        super.checkColumnHasValue(recordIndex, 3, finish);
+        super.checkColumnHasValue(recordIndex, 4, workload);
+        super.checkColumnHasValue(recordIndex, 5, link);
+
         super.clickOnListingRecord(recordIndex);
+        super.checkButtonDoesNotExist("Delete");
+        super.clickOnReturnButton("Return");
 
-        super.checkNotExists(By.linkText("Delete"));
-
+        super.checkColumnHasValue(recordIndex, 0, title);
+        super.checkColumnHasValue(recordIndex, 1, description);
+        super.checkColumnHasValue(recordIndex, 2, start);
+        super.checkColumnHasValue(recordIndex, 3, finish);
+        super.checkColumnHasValue(recordIndex, 4, workload);
+        super.checkColumnHasValue(recordIndex, 5, link);
 
         super.signOut();
     }
