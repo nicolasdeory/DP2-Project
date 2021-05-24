f06497a5c67e59d62ad8514a3a08c697586aebba
Jose Nieves <esoj.nieves@gmail.com>
2021-05-24 18:49:02 +0200

    pos/neg update user account

--- src/test/java/acme/testing/accounts/SignUpTest.java
+++ src/test/java/acme/testing/accounts/SignUpTest.java
@@ -1,22 +1,13 @@
-/*
- * SignUpTest.java
- *
- * Copyright (C) 2012-2021 Rafael Corchuelo.
- *
- * In keeping with the traditional purpose of furthering education and research, it is
- * the policy of the copyright owner to permit non-commercial use and redistribution of
- * this software. It has been tested carefully, but it is not guaranteed for any particular
- * purposes. The copyright owner does not offer any warranties or representations, nor do
- * they accept any liabilities with respect to them.
- */
+package acme.testing.accounts;
 
-package acme.testing;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
+
+import acme.testing.AcmeTest;
 
 class SignUpTest extends AcmeTest {
 
@@ -41,14 +32,17 @@ class SignUpTest extends AcmeTest {
 
 	// Test cases -------------------------------------------------------------
 
-	//En este test recibimos una lista de usuarios que registrar con los datos de usuario, contraseña, nombre, apellido y email.
-	//Primero llamamos a la función signUp para que registre a nuestro usuario y tras ello nos logueamos con los mismos datos
-	//Si todo va bien, veremos que estamos logueados.
-	//Nos deslogueamos y volvemos a hacerlo con todos los usuarios de la lista.
+	// En este test recibimos una lista de usuarios que registrar con los datos de
+	// usuario, contraseña, nombre, apellido y email.
+	// Primero llamamos a la función signUp para que registre a nuestro usuario y
+	// tras ello nos logueamos con los mismos datos
+	// Si todo va bien, veremos que estamos logueados.
+	// Nos deslogueamos y volvemos a hacerlo con todos los usuarios de la lista.
 	@ParameterizedTest
 	@CsvFileSource(resources = "/sign-up/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	void positiveSignUp(final String username, final String password, final String name, final String surname, final String email) {
+	void positiveSignUp(final String username, final String password, final String name, final String surname,
+			final String email) {
 		this.signUp(username, password, name, surname, email);
 		this.signIn(username, password);
 		Assertions.assertTrue(super.exists(By.linkText("Account")), "account doesnt exist");
@@ -56,6 +50,5 @@ class SignUpTest extends AcmeTest {
 	}
 
 	// Ancillary methods ------------------------------------------------------
-
 
 }
---
+++ src/test/java/acme/testing/accounts/UpdateUserAccountTest.java
@@ -0,0 +1,102 @@
+package acme.testing.accounts;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.By;
+
+import acme.testing.AcmeTest;
+
+class UpdateUserAccountTest extends AcmeTest {
+
+    // Internal state ---------------------------------------------------------
+    String actualPassword = "1234";
+    // Lifecycle management ---------------------------------------------------
+
+    @Override
+    @BeforeAll
+    public void beforeAll() {
+        super.beforeAll();
+
+        super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
+        super.setAutoPausing(false);
+
+        this.signIn("administrator", "administrator");
+        super.clickAndGo(By.linkText("Administrator"));
+        super.clickAndGo(By.linkText("Populate DB (initial)"));
+        super.checkAlertExists(true);
+        super.clickAndGo(By.linkText("Administrator"));
+        super.clickAndGo(By.linkText("Populate DB (samples)"));
+        super.checkAlertExists(true);
+        this.sleep(10, true);
+    }
+
+    // Test cases -------------------------------------------------------------
+
+    /*
+     * CSV: positive signup cases;
+     * 
+     * TEST: 1: sign in, 2: go to update account, 3: fill the fields, 4:update, 5:
+     * sign out, 6: sign in updated, 7: check new values
+     */
+    @ParameterizedTest
+    @CsvFileSource(resources = "/sign-up/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+    @Order(10)
+    void positiveUpdate(final String username, final String password, final String name, final String surname,
+            final String email) {
+        this.signIn("juan21", this.actualPassword);
+
+        super.clickOnMenu("Account", "General data");
+
+        super.fillInputBoxIn("password", password);
+        super.fillInputBoxIn("confirmation", password);
+        super.fillInputBoxIn("identity.name", name);
+        super.fillInputBoxIn("identity.surname", surname);
+        super.fillInputBoxIn("identity.email", email);
+
+        super.clickOnSubmitButton("Update");
+
+        this.signOut();
+        this.signIn("juan21", password);
+
+        super.clickOnMenu("Account", "General data");
+
+        super.checkInputBoxHasValue("username", "juan21");
+        super.checkInputBoxHasValue("identity.name", name);
+        super.checkInputBoxHasValue("identity.surname", surname);
+        super.checkInputBoxHasValue("identity.email", email);
+        this.actualPassword = password;
+        this.signOut();
+    }
+
+    /*
+     * CSV: positive signup cases;
+     * 
+     * TEST: 1: sign in, 2: go to update account, 3: fill the fields, 4:update, 5:
+     * sign out, 6: sign in updated, 7: check new values
+     */
+    @ParameterizedTest
+    @CsvFileSource(resources = "/sign-up/negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+    @Order(10)
+    void negativeUpdate(final String username, final String password, final String name, final String surname,
+            final String email) {
+        this.signIn("juan21", this.actualPassword);
+
+        super.clickOnMenu("Account", "General data");
+
+        super.fillInputBoxIn("password", password);
+        super.fillInputBoxIn("confirmation", password);
+        super.fillInputBoxIn("identity.name", name);
+        super.fillInputBoxIn("identity.surname", surname);
+        super.fillInputBoxIn("identity.email", email);
+        super.clickOnSubmitButton("Update");
+
+        super.checkErrorsExist();
+
+        this.signOut();
+    }
+
+    // Ancillary methods ------------------------------------------------------
+
+}
