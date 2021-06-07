b7400a627dabcfac6b2ec68fc2468c43b2782ad7
Pedro Alonso Pontiga <pedropontiga@gmail.com>
2021-06-08 01:11:27 +0200

    XXX create test and list test

--- src/test/java/acme/testing/anonymous/XXX/AnonymousCreateXXXTest.java
+++ src/test/java/acme/testing/anonymous/XXX/AnonymousCreateXXXTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.By;
 
 class AnonymousCreateXXXTest extends AcmeTest {
 
@@ -16,6 +17,10 @@ class AnonymousCreateXXXTest extends AcmeTest {
 
         super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
         super.setAutoPausing(false);
+        this.signIn("administrator", "administrator");
+        super.clickAndGo(By.linkText("Administrator"));
+        super.clickAndGo(By.linkText("Populate DB (initial)"));
+        super.checkAlertExists(true);
 
     }
     // Test cases -------------------------------------------------------------
@@ -26,16 +31,15 @@ class AnonymousCreateXXXTest extends AcmeTest {
     @CsvFileSource(resources = "/anonymous/XXX/CreatePositive.csv", encoding = "utf-8", numLinesToSkip = 1)
     @Order(10)
     void CreatePositive(final int recordIndex, final String author, final String info,
-    	final String text,String date,String currency,String flag) {
+    	final String text,String currency,String flag) {
         super.clickOnMenu("Anonymous", "Shout!");        
         
 
         super.fillInputBoxIn("author", author);
         super.fillInputBoxIn("text", text);
         super.fillInputBoxIn("info", info);
-        super.fillInputBoxIn("date",date);
         super.fillInputBoxIn("currency",currency);
-        super.fillInputBoxIn("flag",flag);
+        if(flag!=null&&flag.equals("true"))super.clickAndGo(By.id("XXXflag$proxy"));
         super.clickOnSubmitButton("Shout!");
         
         
@@ -45,7 +49,6 @@ class AnonymousCreateXXXTest extends AcmeTest {
         super.checkColumnHasValue(recordIndex, 1, author);
         super.checkColumnHasValue(recordIndex, 2, text);
         super.checkColumnHasValue(recordIndex, 3, info);
-        super.checkColumnHasValue(recordIndex,4,date);
         super.checkColumnHasValue(recordIndex,5,currency);
         super.checkColumnHasValue(recordIndex,6,flag);
         
@@ -60,16 +63,15 @@ class AnonymousCreateXXXTest extends AcmeTest {
     @CsvFileSource(resources = "/anonymous/XXX/CreateNegative.csv", encoding = "utf-8", numLinesToSkip = 1)
     @Order(10)
     void CreateNegative(final int recordIndex, final String author, final String info,
-    	final String text,String identifier,String currency,String flag) {
+    	final String text,String currency,String flag) {
     	super.clickOnMenu("Anonymous", "Shout!");        
         
 
         super.fillInputBoxIn("author", author);
         super.fillInputBoxIn("text", text);
         super.fillInputBoxIn("info", info);
-        super.fillInputBoxIn("XdateString",identifier);
         super.fillInputBoxIn("currency",currency);
-        super.fillInputBoxIn("flag",flag);
+        if(flag!=null&&flag.equals("true"))super.clickAndGo(By.id("XXXflag$proxy"));
         super.clickOnSubmitButton("Shout!");
         
         super.checkErrorsExist();
